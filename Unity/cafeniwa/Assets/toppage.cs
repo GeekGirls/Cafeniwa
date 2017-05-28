f159a82c7464d808738fee6e7262c255afed207d
岡部由美 <ebako@kamaboko.local>
2017-05-28 12:10:05 +0900

    cafe_behaviorを追加

---
+++ Unity/cafeniwa/Assets/toppage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class toppage : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update (){
+        	if(Input.GetMouseButtonDown(0) == true){
+        		Debug.Log("touch");
+       			SceneManager.LoadScene("cafe_main");
+    	}
+    }
+}
